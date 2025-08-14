4250063f57c348731a4c97fa9c01a1292b6c8f76
Hugo <swenkkros@gmail.com>
2025-08-15 00:50:45 +0200

    Add logo image and update AppSidebar: introduced a new logo image for the sidebar and removed the TeamSwitcher component from the sidebar header for a cleaner layout.

--- apps/web/src/components/layout/team-switcher.tsx
+++ apps/web/src/components/layout/team-switcher.tsx
@@ -32,24 +32,22 @@ export function TeamSwitcher({
     <SidebarMenu>
       <SidebarMenuItem>
         <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <SidebarMenuButton
-              size='lg'
-              className='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'
-            >
-              <div className='bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg'>
-                <activeTeam.logo className='size-4' />
-              </div>
-              <div className='grid flex-1 text-left text-sm leading-tight'>
-                <span className='truncate font-semibold'>
-                  {activeTeam.name}
-                </span>
-                <span className='truncate text-xs'>{activeTeam.plan}</span>
-              </div>
-              <ChevronsUpDown className='ml-auto' />
-            </SidebarMenuButton>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent
+          {/* <DropdownMenuTrigger asChild> */}
+          <SidebarMenuButton
+            size='lg'
+            className='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'
+          >
+            <div className='bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg'>
+              <activeTeam.logo className='size-4' />
+            </div>
+            <div className='grid flex-1 text-left text-sm leading-tight'>
+              <span className='truncate font-semibold'>{activeTeam.name}</span>
+              <span className='truncate text-xs'>{activeTeam.plan}</span>
+            </div>
+            {/* <ChevronsUpDown className='ml-auto' /> */}
+          </SidebarMenuButton>
+          {/* </DropdownMenuTrigger> */}
+          {/* <DropdownMenuContent
             className='w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg'
             align='start'
             side={isMobile ? 'bottom' : 'right'}
@@ -80,7 +78,7 @@ export function TeamSwitcher({
                 Add Account
               </div>
             </DropdownMenuItem>
-          </DropdownMenuContent>
+          </DropdownMenuContent> */}
         </DropdownMenu>
       </SidebarMenuItem>
     </SidebarMenu>
