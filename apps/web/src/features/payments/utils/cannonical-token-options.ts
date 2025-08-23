4079b6738036f692906d60e193f167e91e161dff
Hugo <102437612+hugopino@users.noreply.github.com>
2025-08-23 18:31:34 +0200

    Refactor Payments component to improve payment selection logic and state management. Introduced new types for checkout selection and canonical token options, enhancing clarity and type safety. Updated token and network handling to streamline user experience, including auto-selection of networks and improved rendering of available options.

---
+++ apps/web/src/features/payments/utils/cannonical-token-options.ts
@@ -0,0 +1,39 @@
+import type {
+  NetworkId,
+  TokenResponseDto,
+} from '@/lib/requests/api-client/model'
+import { CanonicalTokenOption } from '../types/cannonical-token-option'
+
+export function getCanonicalTokenOptions(
+  tokens: TokenResponseDto[] | undefined
+): CanonicalTokenOption[] {
+  if (!tokens) return []
+
+  const map = new Map<
+    string,
+    { id: string; symbol: string; imageUrl: string; networks: Set<NetworkId> }
+  >()
+
+  for (const t of tokens) {
+    const key = t.canonicalTokenId
+    const exist = map.get(key)
+
+    if (!exist) {
+      map.set(key, {
+        id: t.id, // tomamos el primero que aparezca
+        symbol: t.symbol,
+        imageUrl: `/images/tokens/${key.toLowerCase()}.png`,
+        networks: new Set<NetworkId>([t.networkId]),
+      })
+    } else {
+      exist.networks.add(t.networkId)
+    }
+  }
+
+  return Array.from(map.values()).map((o) => ({
+    id: o.id,
+    symbol: o.symbol,
+    imageUrl: o.imageUrl,
+    networks: Array.from(o.networks), // si quieres orden, aplica sort aquí
+  }))
+}
