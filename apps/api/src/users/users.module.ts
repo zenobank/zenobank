1cc1c9af30aeef6b201474723b760a988b76d35a
Hugo <102437612+hugopino@users.noreply.github.com>
2025-09-15 20:51:16 +0200

    Refactor payment and user services to resolve circular dependencies
    - Removed WalletService from PaymentService constructor.
    - Updated UsersModule to import WalletModule and PrismaModule.
    - Removed Svix injection from UsersService constructor for cleaner code.

--- apps/api/src/users/users.module.ts
+++ apps/api/src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { WalletModule } from 'src/wallet/wallet.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   providers: [UsersService],
   controllers: [UsersController],
+  imports: [WalletModule, PrismaModule],
 })
 export class UsersModule {}
