e90317a0fc43f749c7bef57b5f603257cbac30e6
Hugo <102437612+hugopino@users.noreply.github.com>
2025-09-18 18:37:06 +0200

    Refactor OpenAPI Specification and Update Configuration
    - Simplified the OpenAPI specification by consolidating the "tags" array format for various endpoints.
    - Updated the orval configuration to use the new environment variable VITE_API_BASE_URL for the base URL, enhancing flexibility in API endpoint management.

--- apps/web/orval.config.ts
+++ apps/web/orval.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   api: {
     input: '../api/openapi.json',
     output: {
-      baseUrl: process.env.VITE_API_URL + '/api/v1',
+      baseUrl: process.env.VITE_API_BASE_URL + '/api/v1',
       target: 'src/lib/requests/api-client',
       schemas: 'src/lib/requests/api-client/model',
       client: 'react-query',
