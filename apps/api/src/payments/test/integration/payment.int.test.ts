df520ea3f678483a0af38c10966d3ac9a3a837cc
Hugo <102437612+hugopino@users.noreply.github.com>
2025-09-23 12:57:31 +0200

    Enhance payment integration tests by importing PaymentsModule and PaymentService. Update test setup to include necessary providers, improving test reliability and maintainability.

--- apps/api/src/payments/test/integration/payment.int.test.ts
+++ apps/api/src/payments/test/integration/payment.int.test.ts
@@ -1,15 +1,20 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from 'src/app.module';
+import { PaymentsModule } from 'src/payments/payment.module';
+import { PaymentService } from 'src/payments/payment.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('Payment Integration Test', () => {
   let db: PrismaService;
+  let paymentService: PaymentService;
   beforeAll(async () => {
     const testModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [PrismaModule, PaymentsModule],
+      providers: [PrismaService, PaymentService],
     }).compile();
     db = testModule.get(PrismaService);
-    // await db.cleanDb();
+    paymentService = testModule.get(PaymentService);
   });
 
   it('should create a payment', () => {
