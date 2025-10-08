4d006df32d67bcc9700b1b05c03f7a8afce1eef0
Hugo <102437612+hugopino@users.noreply.github.com>
2025-10-08 12:02:06 +0400

    Refactor ConversionsService to enhance currency conversion logic
    - Updated ConversionsService to utilize Iso4217Converter for improved currency conversion accuracy.
    - Refactored conversion method to handle cases where the source currency is not directly supported, using USD as an intermediary.
    - Improved error handling and ensured consistent return types for conversion results.

--- apps/api/src/conversions/conversions.service.ts
+++ apps/api/src/conversions/conversions.service.ts
@@ -5,18 +5,21 @@ import {
   OnModuleInit,
 } from '@nestjs/common';
 import CryptoConvert from 'crypto-convert';
+import { Converter as Iso4217Converter } from 'easy-currencies';
 
 @Injectable()
 export class ConversionsService implements OnModuleInit {
   private readonly logger = new Logger(ConversionsService.name);
-  private convertApi: CryptoConvert;
+  private cryptoConverter: CryptoConvert;
+  private iso4217Converter: Iso4217Converter;
   async onModuleInit() {
-    this.convertApi = new CryptoConvert({
+    this.cryptoConverter = new CryptoConvert({
       binance: true,
       calculateAverage: false,
       kraken: false,
     });
-    await this.convertApi.ready();
+    this.iso4217Converter = new Iso4217Converter();
+    await this.cryptoConverter.ready();
   }
   async convert({
     from,
@@ -31,15 +34,27 @@ export class ConversionsService implements OnModuleInit {
     from: string;
     to: string;
   }> {
-    const result = await this.convertApi[from][to](amount);
-    if (result == null || result < 0 || result === '') {
-      this.logger.error('Error converting currencies', result);
+    from = from.toUpperCase();
+    to = to.toUpperCase();
+    let proxyFrom = from;
+    let proxyAmount = amount;
+    if (!this.cryptoConverter[from]) {
+      const intermediateUSDAmount = await this.iso4217Converter.convert(
+        +amount,
+        from,
+        'USD',
+      );
+      proxyFrom = 'USD';
+      proxyAmount = intermediateUSDAmount.toString();
+    }
+    const result = await this.cryptoConverter[proxyFrom][to](proxyAmount);
+    if (result == null || +result < 0 || result === '') {
       throw new InternalServerErrorException(
         `Error converting currencies.From: ${from} To: ${to} Amount: ${amount}`,
       );
     }
     return {
-      amount: result,
+      amount: result.toString(),
       from,
       to,
     };
