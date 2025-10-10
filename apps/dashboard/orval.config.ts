2b38c680c92b2b5bb528913527c58ad1e938c396
Hugo <102437612+hugopino@users.noreply.github.com>
2025-10-10 20:41:33 +0400

    Remove package-lock.json, add orval configuration files for dashboard and pay apps, and update build scripts to include API client generation using orval.

---
+++ apps/dashboard/orval.config.ts
@@ -0,0 +1,18 @@
+// orval.config.ts
+import * as dotenv from 'dotenv'
+import { defineConfig } from 'orval'
+
+dotenv.config()
+
+export default defineConfig({
+  api: {
+    input: '../api/openapi.json',
+    output: {
+      baseUrl: process.env.API_BASE_URL,
+      target: 'src/lib/generated/index.ts',
+      schemas: 'src/lib/generated/model',
+      client: 'react-query',
+      clean: true,
+    },
+  },
+})
