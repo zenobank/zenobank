b05cdcee39c56a3c835d1c61aafbc6377446ac31
Hugo <102437612+hugopino@users.noreply.github.com>
2025-10-10 19:20:01 +0400

    Update API destination in next.config.ts to use NODE_ENV for dynamic routing based on environment.

--- apps/pay/next.config.ts
+++ apps/pay/next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.API_BASE_URL}/:path*`,
+        destination: `${process.env.NODE_ENV}/:path*`,
       },
     ];
   },
