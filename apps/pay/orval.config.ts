32dd4a2a3af610c5d34a1d70fc541bcb48fc25b3
Hugo <102437612+hugopino@users.noreply.github.com>
2025-10-10 22:45:16 +0400

    Update turbo.json to add API client generation configuration, modify .env.example to set API_URL for local development, and adjust orval.config.ts files in dashboard and pay apps to use API_URL for base URL.

--- apps/pay/orval.config.ts
+++ apps/pay/orval.config.ts
@@ -3,12 +3,11 @@ import * as dotenv from 'dotenv';
 import { defineConfig } from 'orval';
 
 dotenv.config();
-
 export default defineConfig({
   api: {
     input: '../api/openapi.json',
     output: {
-      baseUrl: process.env.API_BASE_URL,
+      baseUrl: process.env.API_URL,
       target: 'src/lib/generated/api-client/index.ts',
       schemas: 'src/lib/generated/api-client/model',
       client: 'react-query',
