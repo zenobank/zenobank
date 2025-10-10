8854927b67d32856ba1299b1591afcd67d04d43f
Hugo <102437612+hugopino@users.noreply.github.com>
2025-10-10 23:15:42 +0400

    Add console log for API_URL in orval.config.ts to aid in debugging environment variable setup.

--- apps/pay/orval.config.ts
+++ apps/pay/orval.config.ts
@@ -3,6 +3,8 @@ import * as dotenv from 'dotenv';
 import { defineConfig } from 'orval';
 
 dotenv.config();
+
+console.log('process.env.API_URL', process.env.API_URL);
 export default defineConfig({
   api: {
     input: '../api/openapi.json',
