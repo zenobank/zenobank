f4ef8c385cf8769df04b216d6253cce860c5bba2
Hugo <102437612+hugopino@users.noreply.github.com>
2025-10-18 20:20:51 +0400

    Refactor PayFooter component for improved layout and support information. Updated link styling and added Telegram support contact.

--- apps/pay/src/features/payments/components/pay-footer.tsx
+++ apps/pay/src/features/payments/components/pay-footer.tsx
@@ -2,12 +2,19 @@ import { envClient } from '@/src/lib/env-client';
 
 export default function PayFooter() {
   return (
-    <div className="mt-6 text-center">
-      <p className="text-xs font-normal">Open Source Crypto Gateway</p>
-      <p className="text-xs font-light italic">
-        Powered by{' '}
-        <a href={`https://zenobank.io`} target="_blank" rel="noreferrer">
-          <span className="font-semibold underline">Zenobank</span>
+    <div className="mt-6 flex flex-col gap-y-1 text-center">
+      <p className="text-xs font-light hover:underline">
+        <a href="https://zenobank.io" target="_blank" rel="noreferrer">
+          <span className="">
+            <span className="font-normal">Zenobank</span> - Open Source Crypto Gateway
+          </span>
+        </a>
+      </p>
+
+      <p className="text-xs font-light">
+        For support, contact us{' '}
+        <a href={`https://t.me/zenobank`} target="_blank" rel="noreferrer">
+          <span className="underline">on Telegram</span>
         </a>
       </p>
     </div>
